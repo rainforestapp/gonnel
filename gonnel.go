fd659dc40c3c202c4a9564f33dd57579be52197b
Emanuel Evans <mail@emanuel.industries>
2020-01-11 00:09:28 -0800

    Try fixing online status checking

--- gonnel.go
+++ gonnel.go
@@ -101,7 +101,7 @@ type Client struct {
 
 // Constant regex that will be used for handling stdout command
 const (
-	ngReady          = `starting web service.*addr=(\d+\.\d+\.\d+\.\d+:\d+)`   // check if ngrok ready
+	ngReady          = `online`                                                // check if ngrok ready
 	ngInUse          = `address already in use`                                // check if port already in use
 	ngSessionLimited = `is limited to (\d+) simultaneous ngrok client session` // Check limit ngrok
 	webURI           = `\d+\.\d+\.\d+\.\d+:\d+`                                // Find client server
